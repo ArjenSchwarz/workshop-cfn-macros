51013c0b98488c94eb44b2b894cb9329374a5867
Arjen Schwarz <developer@arjen.eu>
2019-07-07 17:07:43 +1000

    Step 2 of the workshop

---
+++ node/step2/test/macro.js
@@ -0,0 +1,84 @@
+const macro = require('../macro');
+var assert = require('assert');
+var retError, result, fragment, requestId, generated_resource_name;
+
+describe('handler', function () {
+  context('Macro Returns Values', function () {
+    before('Invoke function', function (done) {
+      fragment = {
+        "Resources": {
+          "S3Bucket": {
+              "Type": "AWS::S3::Bucket"
+            }
+          }
+        };
+      requestId = "testRequest";
+      var event = {
+        'region': 'ap-southeast-2',
+        'requestId': requestId,
+        'fragment': fragment
+      };
+      var context = {
+        functionName: 'NaclExpander'
+      };
+      macro.handler(event, context, function (err, value) {
+        retError = err ;
+        result = value ;
+        done();
+      });
+    });
+    it('Check that success is returned', function () {
+      assert.equal(result.status, "success");
+    });
+    it('Check that requestId is returned', function () {
+      assert.equal(result.requestId, requestId);
+    });
+    it('Check input is passed through', function () {
+      assert.equal(result.fragment, fragment);
+    });
+  });
+  context('Nacl Association is created', function () {
+    before('Invoke function', function (done) {
+      generated_resource_name = 'SubnetANaclPublic';
+      fragment = {"Resources": {"NaclPublic": { "Type": "AWS::EC2::NetworkAcl", "Properties": {"Association": ["SubnetA"]}}}};
+      requestId = "testRequest";
+      var event = {
+        'region': 'ap-southeast-2',
+        'requestId': requestId,
+        'fragment': fragment
+      };
+      var context = {
+        functionName: 'NaclExpander'
+      };
+      macro.handler(event, context, function (err, value) {
+        retError = err ;
+        result = value ;
+        done();
+      });
+    });
+    it('Check that success and requestId are returned', function () {
+      assert.equal(result.status, "success");
+    });
+    it('Check that requestId is returned', function () {
+      assert.equal(result.requestId, requestId);
+    });
+    it('Check all expected resources exist', function () {
+      var expected_resources = ["NaclPublic", generated_resource_name]
+      assert.deepEqual(Object.keys(result.fragment.Resources), expected_resources);
+    });
+    it('Check that correct type is set for association', function () {
+      var expected_resource_type = 'AWS::EC2::SubnetNetworkAclAssociation';
+      assert.equal(result.fragment.Resources[generated_resource_name].Type, expected_resource_type);
+    });
+    it('Check that correct properties are set for association', function () {
+      var properties = result.fragment.Resources[generated_resource_name].Properties;
+      assert.deepEqual(properties.NetworkAclId, {"Ref": "NaclPublic"});
+      assert.deepEqual(properties.SubnetId, {"Ref": "SubnetA"});
+    });
+    it('Check that all Macro specific properties are removed', function () {
+      assert.equal(result.fragment.Resources.NaclPublic.Properties.hasOwnProperty('Association'), false);
+      assert.equal(result.fragment.Resources.NaclPublic.Properties.hasOwnProperty('Inbound'), false);
+      assert.equal(result.fragment.Resources.NaclPublic.Properties.hasOwnProperty('Outbound'), false);
+    });
+  });
+});
---
+++ step2.md
@@ -0,0 +1,41 @@
+# Step 2: Create associations
+
+In this step we will make our Macro do some actual work! Estimated completion time < 1 hour
+
+## 2.1 Associate a NetworkAcl object to Subnets
+
+In this step we want to ensure our Macro can turn this:
+
+```yaml
+  NaclPublic:
+    Type: AWS::EC2::NetworkAcl
+    Properties:
+      VpcId: !Ref VPC
+      Association:
+        - SubnetA
+```
+
+into:
+
+```yaml
+  NaclPublic:
+    Type: AWS::EC2::NetworkAcl
+    Properties:
+      VpcId: !Ref VPC
+
+  SubnetANaclPublic:
+    Type: AWS::EC2::SubnetNetworkAclAssociation
+    Properties:
+      NetworkAclId: !Ref NaclPublic
+      SubnetId: !Ref SubnetA
+```
+
+This requires your Lambda function to create new resources, so how do we do that?
+
+When looking at the fragment in step 1 you may have noticed that all the resources are accessible through the Resources part of the fragment. And while we simply passed through everything that came in there, we can obviously modify the fragment as it comes through. So, that is what you want to do here by adding a `AWS::EC2::SubnetNetworkAclAssociation` for every item in the `Association` property of our `AWS::EC2::NetworkAcl`, and eventually removing the `Association` property itself (as that is not accepted by CloudFormation).
+
+One important note, remember that every resource in CloudFormation needs its own name. The included tests all assume that you will therefore name the associations as `$Subnet$Nacl`, which means in the above example you give the association between `SubnetA` and `NaclPublic` the name `SubnetANaclPublic`. If you don't want to use the tests you can use any naming standard you yourself enjoy, just keep in mind that they have to stay the same between different deployments of the same template.
+
+## Proceed to Step 3
+
+Once you have the above working, please check out branch `step3` of the repository to proceed.
